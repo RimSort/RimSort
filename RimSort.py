816966a121f942bd985657c4fe04ad0b8dc77cec
twstagg <tristinstagg@protonmail.com>
2023-06-10 13:27:54 -0500

    Update version to Alpha 1.0.5.3

--- RimSort.py
+++ RimSort.py
@@ -110,7 +110,7 @@ class MainWindow(QMainWindow):
 
         # Create the main application window
         self.debug_mode = debug_mode
-        self.version_string = "Alpha-v1.0.5.2"
+        self.version_string = "Alpha-v1.0.5.3"
         self.setWindowTitle(f"RimSort {self.version_string}")
         self.setMinimumSize(QSize(1024, 768))
 
