3c3672d9d31c319c03b08bea8e16e259f013caf6
Jeffery Harrell <jefferyharrell@gmail.com>
2023-10-06 13:43:25 -0700

    Add icon caching to speed up drawing mod lists (#176)

--- model/mod_list_item.py
+++ model/mod_list_item.py
@@ -1,6 +1,9 @@
+import os
 from functools import partial
+from pathlib import Path
+
 from logger_tt import logger
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from PySide6.QtCore import QRectF, QSize, Qt, Signal
 from PySide6.QtGui import QFontMetrics, QIcon, QResizeEvent
@@ -83,14 +86,14 @@ class ModListItemInner(QWidget):
             if self.json_data.get("csharp"):
                 self.csharp_icon = QLabel()
                 self.csharp_icon.setPixmap(
-                    QIcon(self.csharp_icon_path).pixmap(QSize(20, 20))
+                    ModListIcons.csharp_icon().pixmap(QSize(20, 20))
                 )
                 self.csharp_icon.setToolTip(
                     "Contains custom C# assemblies (custom code)"
                 )
             else:
                 self.xml_icon = QLabel()
-                self.xml_icon.setPixmap(QIcon(self.xml_icon_path).pixmap(QSize(20, 20)))
+                self.xml_icon.setPixmap(ModListIcons.xml_icon().pixmap(QSize(20, 20)))
                 self.xml_icon.setToolTip("Contains custom content (textures / XML)")
         self.git_icon = None
         if (
@@ -99,13 +102,13 @@ class ModListItemInner(QWidget):
             and not self.json_data.get("steamcmd")
         ):
             self.git_icon = QLabel()
-            self.git_icon.setPixmap(QIcon(self.git_icon_path).pixmap(QSize(20, 20)))
+            self.git_icon.setPixmap(ModListIcons.git_icon().pixmap(QSize(20, 20)))
             self.git_icon.setToolTip("Local mod that contains a git repository")
         self.steamcmd_icon = None
         if self.json_data["data_source"] == "local" and self.json_data.get("steamcmd"):
             self.steamcmd_icon = QLabel()
             self.steamcmd_icon.setPixmap(
-                QIcon(self.steamcmd_icon_path).pixmap(QSize(20, 20))
+                ModListIcons.steamcmd_icon().pixmap(QSize(20, 20))
             )
             self.steamcmd_icon.setToolTip("Local mod that can be used with SteamCMD")
         # Warning icon hidden by default
@@ -114,7 +117,7 @@ class ModListItemInner(QWidget):
             partial(self.toggle_warning_signal.emit, self.json_data["packageid"])
         )
         self.warning_icon_label.setPixmap(
-            QIcon(self.warning_icon_path).pixmap(QSize(20, 20))
+            ModListIcons.warning_icon().pixmap(QSize(20, 20))
         )
         self.warning_icon_label.setHidden(True)
 
@@ -196,11 +199,11 @@ class ModListItemInner(QWidget):
         :return: QIcon object set to the path of the corresponding icon image
         """
         if self.json_data.get("data_source") == "expansion":
-            return QIcon(self.ludeon_icon_path)
+            return ModListIcons.ludeon_icon()
         elif self.json_data.get("data_source") == "local":
-            return QIcon(self.local_icon_path)
+            return ModListIcons.local_icon()
         elif self.json_data.get("data_source") == "workshop":
-            return QIcon(self.steam_icon_path)
+            return ModListIcons.steam_icon()
         else:
             logger.error(
                 f"No type found for ModListItemInner with package id {self.json_data.get('packageid')}"
@@ -230,3 +233,82 @@ class ModListItemInner(QWidget):
         else:
             self.main_label.setText(self.list_item_name)
         return super().resizeEvent(event)
+
+
+class ModListIcons:
+
+    _data_path: str = os.path.join(os.path.dirname(__file__), "../data")
+
+    _ludeon_icon_path: str = str(Path(os.path.join(_data_path, "ludeon_icon.png")).resolve())
+    _local_icon_path: str = str(Path(os.path.join(_data_path, "local_icon.png")).resolve())
+    _steam_icon_path: str = str(Path(os.path.join(_data_path, "steam_icon.png")).resolve())
+    _csharp_icon_path: str = str(Path(os.path.join(_data_path, "csharp.png")).resolve())
+    _xml_icon_path: str = str(Path(os.path.join(_data_path, "xml.png")).resolve())
+    _git_icon_path: str = str(Path(os.path.join(_data_path, "git.png")).resolve())
+    _steamcmd_icon_path: str = str(Path(os.path.join(_data_path, "steamcmd_icon.png")).resolve())
+    _warning_icon_path: str = str(Path(os.path.join(_data_path, "warning.png")).resolve())
+    _error_icon_path: str = str(Path(os.path.join(_data_path, "error.png")).resolve())
+
+    _ludeon_icon: Optional[QIcon] = None
+    _local_icon: Optional[QIcon] = None
+    _steam_icon: Optional[QIcon] = None
+    _csharp_icon: Optional[QIcon] = None
+    _xml_icon: Optional[QIcon] = None
+    _git_icon: Optional[QIcon] = None
+    _steamcmd_icon: Optional[QIcon] = None
+    _warning_icon: Optional[QIcon] = None
+    _error_icon: Optional[QIcon] = None
+
+    @classmethod
+    def ludeon_icon(cls) -> QIcon:
+        if cls._ludeon_icon is None:
+            cls._ludeon_icon = QIcon(cls._ludeon_icon_path)
+        return cls._ludeon_icon
+
+    @classmethod
+    def local_icon(cls) -> QIcon:
+        if cls._local_icon is None:
+            cls._local_icon = QIcon(cls._local_icon_path)
+        return cls._local_icon
+
+    @classmethod
+    def steam_icon(cls) -> QIcon:
+        if cls._steam_icon is None:
+            cls._steam_icon = QIcon(cls._steam_icon_path)
+        return cls._steam_icon
+
+    @classmethod
+    def csharp_icon(cls) -> QIcon:
+        if cls._csharp_icon is None:
+            cls._csharp_icon = QIcon(cls._csharp_icon_path)
+        return cls._csharp_icon
+
+    @classmethod
+    def xml_icon(cls) -> QIcon:
+        if cls._xml_icon is None:
+            cls._xml_icon = QIcon(cls._xml_icon_path)
+        return cls._xml_icon
+
+    @classmethod
+    def git_icon(cls) -> QIcon:
+        if cls._git_icon is None:
+            cls._git_icon = QIcon(cls._git_icon_path)
+        return cls._git_icon
+
+    @classmethod
+    def steamcmd_icon(cls) -> QIcon:
+        if cls._steamcmd_icon is None:
+            cls._steamcmd_icon = QIcon(cls._steamcmd_icon_path)
+        return cls._steamcmd_icon
+
+    @classmethod
+    def warning_icon(cls) -> QIcon:
+        if cls._warning_icon is None:
+            cls._warning_icon = QIcon(cls._warning_icon_path)
+        return cls._warning_icon
+
+    @classmethod
+    def error_icon(cls) -> QIcon:
+        if cls._error_icon is None:
+            cls._error_icon = QIcon(cls._error_icon_path)
+        return cls._error_icon
