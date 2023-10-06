5b8b38c0489270da85fdb8d208d9e302cce11b93
twstagg <tristinstagg@protonmail.com>
2023-10-06 16:31:24 -0500

    Revert to previous ActiveModList behavior for all_mods

--- view/main_content_panel.py
+++ view/main_content_panel.py
@@ -1074,9 +1074,17 @@ class MainContent(QObject):
                 ),
                 text="Scanning mod sources and populating metadata...",
             )
-
-            # self.metadata_manager.refresh_cache()
-
+            # Set the game version string in the UI and pass it to the active_mods_panel
+            self.game_configuration.game_version_line.setText(
+                self.metadata_manager.game_version
+            )
+            self.active_mods_panel.game_version = self.metadata_manager.game_version
+            # Feed all_mods and Steam DB info to Active Mods list to surface
+            # names instead of package_ids when able
+            self.active_mods_panel.all_mods = self.metadata_manager.all_mods_compiled
+            self.active_mods_panel.steam_package_id_to_name = (
+                self.metadata_manager.info_from_steam_package_id_to_name
+            )
             # Insert mod data into list
             self.__repopulate_lists(is_initial=is_initial)
 
@@ -1113,17 +1121,6 @@ class MainContent(QObject):
             logger.debug(
                 "Essential paths have not been set. Passing refresh and resetting mod lists"
             )
-        # Set the game version string in the UI and pass it to the active_mods_panel
-        self.game_configuration.game_version_line.setText(
-            self.metadata_manager.game_version
-        )
-        self.active_mods_panel.game_version = self.metadata_manager.game_version
-        # Feed all_mods and Steam DB info to Active Mods list to surface
-        # names instead of package_ids when able
-        self.active_mods_panel.all_mods = self.metadata_manager.all_mods_compiled
-        self.active_mods_panel.steam_package_id_to_name = (
-            self.metadata_manager.info_from_steam_package_id_to_name
-        )
 
     def _do_refresh_animation(self, path: str) -> None:
         logger.debug(f"File change detected: {path}")
