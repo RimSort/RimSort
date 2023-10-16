28c75efb1677edb29cf04986cef9125d910ef1af
twstagg <tristinstagg@protonmail.com>
2023-10-16 18:09:30 -0500

    Make use of kebab icon unless path supplied. Styling!

--- model/multibutton.py
+++ model/multibutton.py
@@ -1,7 +1,9 @@
 from functools import partial
+import os
+from pathlib import Path
 from typing import Dict
 
-from PySide6.QtCore import Signal
+from PySide6.QtCore import Qt, Signal
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QPushButton, QToolButton, QMenu
 
@@ -13,11 +15,27 @@ class MultiButton(QWidget):
         main_action_name: str,
         main_action_tooltip: str,
         context_menu_content: Dict[str, str],
+        secondary_action_icon_path=None,
     ):
         super().__init__()
 
+        # Set the object name for styling
+        self.setObjectName("MultiButton")
+
+        # Use custom icon path, or use default kebab icons (Thanks Cousax)
+        self.secondary_action_icon_path = (
+            secondary_action_icon_path
+            if secondary_action_icon_path
+            else str(
+                Path(
+                    os.path.join(os.path.dirname(__file__), "../data/kebab.png")
+                ).resolve()
+            )
+        )
+
         # Create a horizontal layout
         layout = QHBoxLayout(self)
+        layout.setSpacing(0)
         layout.setContentsMargins(0, 0, 0, 0)
 
         # Create a QPushButton for the main action
@@ -27,8 +45,12 @@ class MultiButton(QWidget):
 
         # Create a QToolButton with a menu for the secondary action
         self.secondary_action = QToolButton(self)
-        self.secondary_action.setIcon(QIcon(""))
+        self.secondary_action.setIcon(QIcon(self.secondary_action_icon_path))
         self.secondary_action.setPopupMode(QToolButton.InstantPopup)
+        # Set the style to show the icon only - we don't need the icon applied from the InstantPopup attribute
+        self.secondary_action.setStyleSheet(
+            "QToolButton::menu-indicator { image: none; }"
+        )
         layout.addWidget(self.secondary_action)
 
         # Create the context menu
