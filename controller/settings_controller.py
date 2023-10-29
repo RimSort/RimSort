2409d4636a557ea1261f3931c83043e45ecdebe8
Jeffery Harrell <jefferyharrell@gmail.com>
2023-10-28 18:18:18 -0700

    Document the SettingsController class

--- controller/settings_controller.py
+++ controller/settings_controller.py
@@ -4,7 +4,31 @@ from model.settings import Settings
 
 
 class SettingsController(QObject):
+    """
+    Controller class to manage interactions with the `Settings` model.
+
+    The `SettingsController` class provides a clear interface for working with the `Settings` model.
+    It ensures that the associated settings model is loaded upon initialization.
+
+    Attributes:
+        settings (Settings): The underlying settings model managed by this controller.
+
+    Examples:
+        >>> settings_model = Settings()
+        >>> controller = SettingsController(settings_model)
+        >>> controller.settings.some_property
+    """
+
     def __init__(self, model: Settings) -> None:
+        """
+        Initialize the `SettingsController` with the given `Settings` model.
+
+        Upon initialization, the provided settings model's `load` method is called to ensure
+        that the settings are loaded and available for use.
+
+        Args:
+            model (Settings): The settings model to be managed by this controller.
+        """
         super().__init__()
 
         self.settings = model
