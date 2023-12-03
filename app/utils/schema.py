dad9a84827d3d8b6a190c31e4d25b2b4a2f860f9
twstagg <tristinstagg@protonmail.com>
2023-12-02 20:34:51 -0600

    Add ability to import mod list from .rws files (RimWorld save)
    * Also implements xmltodict as primary xml parser, with bs4 as a fallback for trickier chars/encodings

--- app/utils/schema.py
+++ app/utils/schema.py
@@ -1,41 +1,43 @@
 from loguru import logger
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from app.models.dialogue import show_warning
 
 
-def validate_mods_config_format(mods_config_data: Dict[str, Any]) -> bool:
+def validate_rimworld_mods_list(
+    mods_config_data: Dict[str, Any]
+) -> Optional[list[str]]:
     """
     Validate format of a Rimworld ModsConfig.xml
 
     :return: True if the ModsConfig is in the expected format, otherwise False.
     """
-    logger.debug(f"Validating ModsConfig.xml")
+    logger.debug(f"Validating RimWorld mods list")
     if mods_config_data:
-        if mods_config_data.get("ModsConfigData"):
-            if mods_config_data["ModsConfigData"].get("activeMods"):
-                if mods_config_data["ModsConfigData"]["activeMods"].get("li"):
-                    logger.info("Validated ModsConfig.xml")
-                    return True
-        logger.error(f"Invalid ModsConfig.xml format: {mods_config_data}")
-        show_warning(
-            text="Invalid ModsConfig.xml format",
-            information=(
-                "RimSort was unable to read your ModsConfig.xml because it is incorrectly formatted. "
-                "You may have to re-create the file by deleting it and running RimWorld. See details "
-                "for what RimSort was able to read from your file."
-            ),
-            details=str(mods_config_data),
-        )
-    else:
-        show_warning(
-            text="Missing ModsConfig.xml",
-            information=(
-                "RimSort was unable to read your ModsConfig.xml because it may be missing. "
-                "Please check that a file called ModsConfig.xml exists in the game install "
-                "directory that you have set."
-            ),
-            details=str(mods_config_data),
-        )
-        logger.error(f"Empty mods config data: {mods_config_data}")
+        # ModsConfig.xml format
+        if (
+            mods_config_data.get("ModsConfigData", {})
+            .get("activeMods", {})
+            .get("li", {})
+        ):
+            logger.info("Validated XML formatting (ModsConfig.xml style)")
+            return mods_config_data["ModsConfigData"]["activeMods"]["li"]
+        # RimWorld .rws save file format
+        elif (
+            mods_config_data.get("savegame", {})
+            .get("meta", {})
+            .get("modIds", {})
+            .get("li", {})
+        ):
+            logger.info("Validated XML formatting (RimWorld savegame style)")
+            return mods_config_data["savegame"]["meta"]["modIds"]["li"]
+        else:
+            logger.error(f"Invalid format: {mods_config_data}")
+    show_warning(
+        text="Unable to read data from file",
+        information=(
+            "RimSort was unable to read the supplied mods list because it may be invalid or missing."
+        ),
+        details=str(mods_config_data),
+    )
     return False
--- app/utils/xml.py
+++ app/utils/xml.py
@@ -1,48 +1,63 @@
 import os
 from typing import Any, Dict
-
 from bs4 import BeautifulSoup
 import xmltodict
-
 from loguru import logger
 
 
 def xml_path_to_json(path: str) -> Dict[str, Any]:
     """
-    Return the contents of an xml file as json.
+    Return the contents of an XML file as JSON.
     If the file does not exist, return an empty dict.
 
-    :param path: path to the xml file
-    :return: json dict of xml file contents
+    :param path: Path to the XML file.
+    :return: JSON dict of XML file contents.
     """
-    data: Dict[str, Any] = {}
-    if os.path.exists(path):
-        logger.debug(f"Parsing XML file at: {path}")
-        with open(path, "rb") as f:
-            xml_data = f.read()
-            soup = BeautifulSoup(xml_data, "lxml-xml")
-            # Find and remove empty tags
-            empty_tags = soup.find_all(
-                lambda tag: not tag.text.strip() or len(tag) == 0
-            )
-            for empty_tag in empty_tags:
-                empty_tag.extract()
-            data = xmltodict.parse(str(soup), dict_constructor=dict)
-            logger.debug(f"XML file parsed")
-    else:
+    if not os.path.exists(path):
         logger.error(f"XML file does not exist at: {path}")
+        return {}
+
+    logger.debug(f"Parsing XML file at: {path}")
+    try:
+        try:
+            with open(path, "rb") as file:
+                data = xmltodict.parse(file.read(), dict_constructor=dict)
+        except Exception as e:
+            logger.error(f"Error parsing XML file with xmltodict: {e}")
+            logger.debug("Trying to parse with bs4 as a fallback")
+            with open(path, "rb") as f:
+                xml_data = f.read()
+                soup = BeautifulSoup(xml_data, "lxml-xml")
+                # Find and remove empty tags
+                empty_tags = soup.find_all(
+                    lambda tag: not tag.text.strip() or len(tag) == 0
+                )
+                for empty_tag in empty_tags:
+                    empty_tag.extract()
+                data = xmltodict.parse(str(soup), dict_constructor=dict)
+                logger.debug(f"XML file parsed")
+    except Exception as e:
+        logger.error(f"Error parsing XML file with bs4: {e}")
+        return {}
+
+    logger.debug("XML file parsed successfully")
     return data
 
 
 def json_to_xml_write(data: Dict[str, Any], path: str) -> None:
     """
-    Write json data as an xml file.
+    Write JSON data as an XML file.
 
-    :param data: json data to write
-    :param path: path to write the xml file to
+    :param data: JSON data to write.
+    :param path: Path to write the XML file to.
     """
     logger.debug("Started writing JSON to XML")
-    new_xml_data = xmltodict.unparse(data, pretty=True, newl="\n", indent="  ")
-    with open(path, "w", encoding="utf-8") as f:
-        f.write(new_xml_data)
+    try:
+        xml_data = xmltodict.unparse(data, pretty=True)
+        with open(path, "w", encoding="utf-8") as file:
+            file.write(xml_data)
+    except Exception as e:
+        logger.error(f"Error writing XML file: {e}")
+        return
+
     logger.debug("Finished writing JSON to XML")
