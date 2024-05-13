0d87489d80c2e9c39819551b250043c3efd10c8c
twstagg <tristinstagg@protonmail.com>
2024-05-12 22:18:02 -0500

    Functional instance cloning

--- app/controllers/menu_bar_controller.py
+++ app/controllers/menu_bar_controller.py
@@ -101,6 +101,12 @@ class MenuBarController(QObject):
 
         # Instances menu
 
+        self.menu_bar.clone_instance_action.triggered.connect(
+            partial(
+                EventBus().do_clone_existing_instance.emit,
+                self.settings_controller.settings.current_instance,
+            )
+        )
         self.menu_bar.create_instance_action.triggered.connect(
             EventBus().do_create_new_instance.emit
         )
