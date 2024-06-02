ea2fb5ffb972db7ecc837718ec686ce382494a7e
Kat <28567881+hk21702@users.noreply.github.com>
2024-06-02 07:09:23 -0400

    Non python lint fixes (#370)
    * autoformat
    
    * fix shellchecker things
    
    * ignore license
    
    * fix links
    
    * Add links into the wiki
    
    * format
    
    * direct users to wiki before making bug report
    
    * Add shfmt to wiki
    
    * format
    
    * remove dispatch for test builds
    
    * only trigger lint on push to main to avoid duplicates for pr
    
    * use tabs to comply with shfmt
    
    * Bump wiki commit
    
    * bump wiki version
    
    * update wiki
    add to dictionary
    
    * Revert "use tabs to comply with shfmt"
    
    This reverts commit 425a99488e1e5fcfe298c77b4790323798c66ecb.
    
    * disable shfmt
    
    * validate bash executable
    
    * Revert license change
    
    * Revert "validate bash executable"
    
    This reverts commit 551012fc9629d7b2096e703170b7d96c59712725.

--- update.sh
+++ update.sh
@@ -21,7 +21,7 @@ else
 fi
 
 # Display a message indicating the update operation is starting in 5 seconds
-read -t 5 -p "Updating RimSort in 5 seconds. Press any key to cancel."
+read -tr 5 -p "Updating RimSort in 5 seconds. Press any key to cancel."
 
 # Execute RimSort from the current directory
 if [ "$os" = "Darwin" ]; then # macOS detected
@@ -35,5 +35,6 @@ else # Assume Linux if not macOS
     rm -rf "${parent_dir}"
     # Move files from the update source folder to the current directory
     chmod +x "${update_source_folder}/${executable_name}" && chmod +x "${update_source_folder}/todds/todds" && mv "${update_source_folder}" "${parent_dir}"
-    cd "${parent_dir}" && ./$executable_name & cd "${parent_dir}"
+    cd "${parent_dir}" && ./$executable_name &
+    cd "${parent_dir}" || echo "Failed to cd to ${parent_dir}" && exit 1
 fi
