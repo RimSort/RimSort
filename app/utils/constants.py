ba71333e474d5a54d46fcae8683c5c6a3003482d
kat <28567881+hk21702@users.noreply.github.com>
2024-06-23 05:26:42 -0400

    Remove unused DEFAULT_SETTINGS constant

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -15,42 +15,6 @@ MOD_RECURSE_EXCEPTIONS = [
     "loadbefore",
     "moddependencies",
 ]
-DEFAULT_SETTINGS = {
-    "check_for_update_startup": False,
-    "show_folder_rows": True,
-    "sorting_algorithm": "Alphabetical",
-    "external_steam_metadata_file_path": "steamDB.json",
-    "external_steam_metadata_repo": "https://github.com/RimSort/Steam-Workshop-Database",
-    "external_steam_metadata_source": "None",
-    "external_community_rules_file_path": "communityRules.json",
-    "external_community_rules_repo": "https://github.com/RimSort/Community-Rules-Database",
-    "external_community_rules_metadata_source": "None",
-    "db_builder_include": "all_mods",
-    "database_expiry": 604800,
-    "build_steam_database_dlc_data": True,
-    "build_steam_database_update_toggle": False,
-    "watchdog_toggle": True,
-    "mod_type_filter_toggle": True,
-    "duplicate_mods_warning": False,
-    "steam_mods_update_check": False,
-    "try_download_missing_mods": False,
-    "steamcmd_validate_downloads": True,
-    "todds_preset": "optimized",
-    "todds_active_mods_target": True,
-    "todds_dry_run": False,
-    "todds_overwrite": False,
-    "current_instance": "Default",
-    "instances": {
-        "Default": {
-            "game_folder": "",
-            "config_folder": "",
-            "local_folder": "",
-            "workshop_folder": "",
-            "run_args": [],
-            "steamcmd_install_path": "",
-        }
-    },
-}
 DEFAULT_USER_RULES = {"timestamp": 0, "rules": {}}
 RIMWORLD_DLC_METADATA = {
     "294100": {
