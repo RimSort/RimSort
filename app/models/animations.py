f57e510dc2e9ad926fca06da7e6598a1f7727921
Kat <28567881+hk21702@users.noreply.github.com>
2024-07-15 23:58:29 -0400

    Refactor AnimationLabel and LoadingAnimation classes (#490)

--- app/models/animations.py
+++ app/models/animations.py
@@ -1,5 +1,5 @@
 import traceback
-from typing import Callable
+from typing import Any, Callable
 
 from loguru import logger
 from PySide6.QtCore import QEasingCurve, QPropertyAnimation, Qt, QThread, QTimer, Signal
@@ -28,7 +28,9 @@ class AnimationLabel(QLabel):
         self.effect.setOpacity(0)
         self.setGraphicsEffect(self.effect)
         self.animation = QPropertyAnimation(self.effect, b"opacity")
-        self.timer = QTimer(interval=1000, singleShot=True)
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.setSingleShot(True)
         self.timer.timeout.connect(self.fade)
 
     def fade(self) -> None:
@@ -51,7 +53,7 @@ class AnimationLabel(QLabel):
         :param text: the string to display and fade
         """
         self.clear()
-        if self.timer.isActive:
+        if self.timer.isActive():
             self.timer.stop()
             self.animation.stop()
         self.setText(text)
@@ -63,47 +65,58 @@ class AnimationLabel(QLabel):
 class LoadingAnimation(QWidget):
     finished = Signal()
 
-    def __init__(self, gif_path: str, target: Callable):
+    def __init__(self, gif_path: str, target: Callable[..., Any]):
         super().__init__()
         logger.debug("Initializing LoadingAnimation")
+
         # Store data
         self.animation_finished = False
-        self.data = {}
+        self.data: dict[Any, Any] = {}
+
         # Setup thread
         self.gif_path = gif_path
-        self.thread = WorkThread(parent=self, target=target)
-        self.thread.data_ready.connect(self.handle_data)
-        self.thread.finished.connect(self.prepare_stop_animation)
-        self.thread.start()
+        self._thread = WorkThread(parent=self, target=target)
+        self._thread.data_ready.connect(self.handle_data)
+        self._thread.finished.connect(self.prepare_stop_animation)
+        self._thread.start()
+
         # Window properties
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
+
         # Label and layout
-        self.layout = QVBoxLayout(self)
+        self._layout = QVBoxLayout(self)
         self.movie = QMovie(self.gif_path)
         self.movie.frameChanged.connect(self.check_animation_stop)
         self.movie.start()
-        self.label = QLabel(alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.label = QLabel()
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.label.setMovie(self.movie)
-        self.layout.addWidget(self.label)
-        self.setLayout(self.layout)
+        self._layout.addWidget(self.label)
+        self.setLayout(self._layout)
+
         logger.debug("Finished LoadingAnimation initialization")
 
-    def check_animation_stop(self, frameNumber: int):
+    def check_animation_stop(self, frameNumber: int) -> None:
+        """Checks if the animation should stop."""
         if self.animation_finished and frameNumber == self.movie.frameCount() - 1:
             logger.debug("Animation is finished")
             self.stop_animation()
 
-    def handle_data(self, data):
+    def handle_data(self, data: dict[Any, Any]) -> None:
+        """Handle data received from thread."""
         if data:
             logger.debug(f"Received {type(data)} from thread")
             self.data = data
 
-    def prepare_stop_animation(self):
+    def prepare_stop_animation(self) -> None:
+        """Prepare to stop the animation."""
         # Set flag when thread finished
         logger.debug("Flagging animation to complete")
         self.animation_finished = True
 
-    def stop_animation(self):
+    def stop_animation(self) -> None:
+        """Stop the animation."""
         logger.debug("Stopping animation")
         # Stop animation
         self.label.clear()
@@ -115,12 +128,12 @@ class LoadingAnimation(QWidget):
 class WorkThread(QThread):
     data_ready = Signal(object)
 
-    def __init__(self, target: Callable, parent=None):
+    def __init__(self, target: Callable[..., Any], parent: Any = None) -> None:
         QThread.__init__(self)
         self.data = None
         self.target = target
 
-    def run(self):
+    def run(self) -> None:
         try:
             self.data = self.target()
         except Exception as e:
