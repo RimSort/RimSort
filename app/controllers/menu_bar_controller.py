f8f66a2b378b097767c2700445308c1f231b9b74
Lionel Colaso <lionelcolaso@outlook.com>
2024-10-15 17:21:13 +0530

    Additional Shortcuts and Improved Organization #513
     1) Added additional shortcuts under File > Open... > RimWorld
        Root Directory
        Config Directory
        Local Mods Directory
        Steam Mods Directory
    
    2) Organized shortcuts such that related shortcuts are grouped under an additional folder
       RimSort
       RimWorld

--- app/controllers/menu_bar_controller.py
+++ app/controllers/menu_bar_controller.py
@@ -12,7 +12,12 @@ from app.views.menu_bar import MenuBar
 
 
 class MenuBarController(QObject):
-    def __init__(self, view: MenuBar, settings_controller: SettingsController, mods_panel_controller: ModsPanelController) -> None:
+    def __init__(
+        self,
+        view: MenuBar,
+        settings_controller: SettingsController,
+        mods_panel_controller: ModsPanelController,
+    ) -> None:
         super().__init__()
 
         self.menu_bar = view
@@ -43,80 +48,76 @@ class MenuBarController(QObject):
         )
 
         # File menu
-
         self.menu_bar.open_mod_list_action.triggered.connect(
             EventBus().do_open_mod_list.emit
         )
-
         self.menu_bar.save_mod_list_action.triggered.connect(
             EventBus().do_save_mod_list_as.emit
         )
-
         self.menu_bar.import_from_rentry_action.triggered.connect(
             EventBus().do_import_mod_list_from_rentry
         )
-
         self.menu_bar.import_from_workshop_collection_action.triggered.connect(
             EventBus().do_import_mod_list_from_workshop_collection
         )
-
         self.menu_bar.export_to_clipboard_action.triggered.connect(
             EventBus().do_export_mod_list_to_clipboard
         )
-
         self.menu_bar.export_to_rentry_action.triggered.connect(
             EventBus().do_export_mod_list_to_rentry
         )
         self.menu_bar.upload_rimsort_log_action.triggered.connect(
             EventBus().do_upload_rimsort_log
         )
-
         self.menu_bar.upload_rimsort_old_log_action.triggered.connect(
             EventBus().do_upload_rimsort_old_log
         )
-
         self.menu_bar.upload_rimworld_log_action.triggered.connect(
             EventBus().do_upload_rimworld_log
         )
 
+        # Shortcuts SubMenu
         self.menu_bar.open_app_directory_action.triggered.connect(
             EventBus().do_open_app_directory
         )
-
         self.menu_bar.open_settings_directory_action.triggered.connect(
             EventBus().do_open_settings_directory
         )
-
         self.menu_bar.open_rimsort_logs_directory_action.triggered.connect(
             EventBus().do_open_rimsort_logs_directory
         )
-
+        self.menu_bar.open_rimworld_directory_action.triggered.connect(
+            EventBus().do_open_rimworld_directory
+        )
+        self.menu_bar.open_rimworld_config_directory_action.triggered.connect(
+            EventBus().do_open_rimworld_config_directory
+        )
         self.menu_bar.open_rimworld_logs_directory_action.triggered.connect(
             EventBus().do_open_rimworld_logs_directory
         )
+        self.menu_bar.open_local_mods_directory_action.triggered.connect(
+            EventBus().do_open_local_mods_directory
+        )
+        self.menu_bar.open_steam_mods_directory_action.triggered.connect(
+            EventBus().do_open_steam_mods_directory
+        )
 
         # Edit menu
-
         self.menu_bar.cut_action.triggered.connect(self._on_menu_bar_cut_triggered)
-
         self.menu_bar.copy_action.triggered.connect(self._on_menu_bar_copy_triggered)
-
         self.menu_bar.paste_action.triggered.connect(self._on_menu_bar_paste_triggered)
-
         self.menu_bar.rule_editor_action.triggered.connect(EventBus().do_rule_editor)
-
-        self.menu_bar.reset_all_warnings_action.triggered.connect(self._on_reset_warnings_triggered)
+        self.menu_bar.reset_all_warnings_action.triggered.connect(
+            self._on_reset_warnings_triggered
+        )
 
         # Download menu
-
         self.menu_bar.add_git_mod_action.triggered.connect(
             EventBus().do_add_git_mod.emit
         )
-
         self.menu_bar.browse_workshop_action.triggered.connect(
             EventBus().do_browse_workshop
         )
-
         self.menu_bar.update_workshop_mods_action.triggered.connect(
             EventBus().do_check_for_workshop_updates
         )
@@ -139,24 +140,19 @@ class MenuBarController(QObject):
         )
 
         # Textures menu
-
         self.menu_bar.optimize_textures_action.triggered.connect(
             EventBus().do_optimize_textures
         )
-
         self.menu_bar.delete_dds_textures_action.triggered.connect(
             EventBus().do_delete_dds_textures
         )
-
         # Help menu
-
         self.menu_bar.wiki_action.triggered.connect(self._on_menu_bar_wiki_triggered)
         self.menu_bar.validate_steam_client_action.triggered.connect(
             EventBus().do_validate_steam_client
         )
 
         # External signals
-
         EventBus().refresh_started.connect(self._on_refresh_started)
         EventBus().refresh_finished.connect(self._on_refresh_finished)
 
