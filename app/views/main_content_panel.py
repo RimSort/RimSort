298c844aa4f6ae433f2eca69bab1fe1c4bad1f3f
Patryk Malinowski <145374860+Patryk-Malinowski@users.noreply.github.com>
2025-02-17 13:38:13 +0000

    Update docstring

--- app/views/main_content_panel.py
+++ app/views/main_content_panel.py
@@ -586,11 +586,17 @@ class MainContent(QObject):
     def __mod_list_slot(self, uuid: str, item: CustomListWidgetItem) -> None:
         """
         This slot method is triggered when the user clicks on an item
-        on a mod list. It takes the internal uuid and gets the
+        on a mod list.
+        
+        It takes the internal uuid and gets the
         complete json mod info for that internal uuid. It passes
         this information to the mod info panel to display.
 
+        It also takes the selected mod (CustomListWidgetItem) and passes 
+        this to the mod info panel to display that mod's notes.
+
         :param uuid: uuid of mod
+        :param item: selected CustomListWidgetItem 
         """
         self.mod_info_panel.display_mod_info(
             uuid=uuid,
