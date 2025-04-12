f4ec92f712c637b4c50cba1cf22bf474ecd2eb61
Lionel Colaso <lionelcolaso@outlook.com>
2025-04-12 16:27:24 +0530

    mods_panel create mods_panel_list_widget.py and move class ModListWidget to it

--- app/views/main_content_panel.py
+++ app/views/main_content_panel.py
@@ -68,7 +68,8 @@ from app.utils.system_info import SystemInfo
 from app.utils.todds.wrapper import ToddsInterface
 from app.utils.xml import json_to_xml_write
 from app.views.mod_info_panel import ModInfo
-from app.views.mods_panel import ModListWidget, ModsPanel
+from app.views.mods_panel import ModsPanel
+from app.views.mods_panel_list_widget import ModListWidget
 from app.windows.missing_dependencies_dialog import MissingDependenciesDialog
 from app.windows.missing_mods_panel import MissingModsPrompt
 from app.windows.rule_editor_panel import RuleEditor
