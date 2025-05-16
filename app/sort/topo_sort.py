3d7154660a6efd6b56148457c40d99d682307e90
Lionel Colaso <lionelcolaso@outlook.com>
2025-05-16 07:52:47 +0530

    topo_sort.py: Add Docstring and Minor Refactor

--- app/sort/topo_sort.py
+++ app/sort/topo_sort.py
@@ -13,8 +13,12 @@ def do_topo_sort(
     """
     Sort mods using the topological sort algorithm. For each
     topological level, sort the mods alphabetically.
+
+    :param dependency_graph: Dependency graph mapping package IDs to dependencies
+    :param active_mods_uuids: Set of active mod UUIDs
+    :return: List of sorted mod UUIDs
     """
-    logger.info(f"Initializing toposort for {len(dependency_graph)} mods")
+    logger.info(f"Initializing topological sort for {len(dependency_graph)} mods")
     # Cache MetadataManager instance
     metadata_manager = MetadataManager.instance()
     try:
@@ -25,16 +29,21 @@ def do_topo_sort(
         raise e
 
     reordered = list()
+    # Build a map of dependency package_id to name for active mods
     active_mods_packageid_to_uuid = dict(
-        (metadata_manager.internal_local_metadata[uuid]["packageid"], uuid)
+        (
+            metadata_manager.internal_local_metadata[uuid]["packageid"],
+            uuid,
+        )
         for uuid in active_mods_uuids
     )
+    # Iterate through the sorted dependencies
     for level in sorted_dependencies:
-        temp_mod_set = set()
-        for package_id in level:
-            if package_id in active_mods_packageid_to_uuid:
-                mod_uuid = active_mods_packageid_to_uuid[package_id]
-                temp_mod_set.add(mod_uuid)
+        temp_mod_set = set(
+            active_mods_packageid_to_uuid[package_id]
+            for package_id in level
+            if package_id in active_mods_packageid_to_uuid
+        )
         # Sort packages in this topological level by name
         sorted_temp_mod_set = sorted(
             temp_mod_set,
@@ -42,17 +51,22 @@ def do_topo_sort(
             reverse=False,
         )
         # Add into reordered set
-        for sorted_mod_uuid in sorted_temp_mod_set:
-            reordered.append(sorted_mod_uuid)
-    logger.info(f"Finished Toposort sort with {len(reordered)} mods")
+        reordered.extend(sorted_temp_mod_set)
+    logger.info(f"Finished topological sort with {len(reordered)} mods")
     return reordered
 
 
 def find_circular_dependencies(dependency_graph: dict[str, set[str]]) -> None:
+    """
+    Detect and log circular dependencies in the given dependency graph.
+    Show a warning dialog with details if any cycles are found.
+
+    :param dependency_graph: Dependency graph mapping package IDs to dependencies
+    """
     graph = nx.DiGraph(dependency_graph)  # type: ignore # Stubs seem to be broken for args
     cycles = list(nx.simple_cycles(graph))
 
-    cycle_strings = []
+    cycle_strings = list()
     if cycles:
         logger.info("Circular dependencies detected:")
         for cycle in cycles:
