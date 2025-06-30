758a1ee568ee663b448ac5a4a03a954cdc5c6a44
Lionel Colaso <lionelcolaso@outlook.com>
2025-06-30 15:24:14 +0530

    refactor(settings): disable database expiry by default

--- app/models/settings.py
+++ app/models/settings.py
@@ -45,7 +45,8 @@ class Settings(QObject):
             "https://github.com/RimSort/Community-Rules-Database"
         )
 
-        self.database_expiry: int = 604800  # 7 days
+        # Disable by default previously this was 7 days "604800"
+        self.database_expiry: int = 0
 
         self.external_no_version_warning_metadata_source: str = "None"
         self.external_no_version_warning_file_path: str = str(
