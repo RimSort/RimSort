0cc5cd65639f4cc99735265b52190a077f5ca192
Lionel Colaso <lionelcolaso@outlook.com>
2025-07-02 19:27:30 +0530

    Fix type hints for qtbot.mouseClick calls in test_dialogue.py

--- tests/views/test_dialogue.py
+++ tests/views/test_dialogue.py
@@ -182,14 +182,14 @@ class TestBinaryChoiceDialog:
     def test_click(self, qtbot: QtBot) -> None:
         dialog1 = BinaryChoiceDialog()
         qtbot.addWidget(dialog1)
-        qtbot.mouseClick(
+        qtbot.mouseClick(  # type: ignore
             dialog1.button(dialog1.positive_btn), Qt.MouseButton.LeftButton
         )
         assert dialog1.result() == dialog1.positive_btn
 
         dialog2 = BinaryChoiceDialog()
         qtbot.addWidget(dialog2)
-        qtbot.mouseClick(
+        qtbot.mouseClick(  # type: ignore
             dialog2.button(dialog2.negative_btn), Qt.MouseButton.LeftButton
         )
         assert dialog2.result() == dialog2.negative_btn
