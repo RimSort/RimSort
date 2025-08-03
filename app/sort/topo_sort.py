cd09bbb19dc769830d087b72b342f9548def7cae
hk21702 <28567881+hk21702@users.noreply.github.com>
2025-08-02 22:01:53 -0400

    fix: Add type ignore comment for networkx DiGraph instantiation

--- app/sort/topo_sort.py
+++ app/sort/topo_sort.py
@@ -58,7 +58,7 @@ def do_topo_sort(
 
 
 def find_circular_dependencies(dependency_graph: dict[str, set[str]]) -> None:
-    graph = nx.DiGraph(dependency_graph)  # use the networkx library
+    graph = nx.DiGraph(dependency_graph)  # type: ignore # Type checkers want a iterable, but a set is fine.
     cycles = list(nx.simple_cycles(graph))  # find all cycles in the graph
 
     cycle_strings = []
