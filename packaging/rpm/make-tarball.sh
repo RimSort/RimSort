a1cdd7281d27ab8e0bb1a47a160c5cd8db23a658
Anten Skrabec <cebarks@gmail.com>
2025-12-20 15:28:13 -0700

    fix bash linting errors

--- packaging/rpm/make-tarball.sh
+++ packaging/rpm/make-tarball.sh
@@ -10,13 +10,13 @@ echo "Creating source tarball with submodules..."
 
 # Create temporary directory
 TMPDIR=$(mktemp -d)
-trap "rm -rf $TMPDIR" EXIT
+trap 'rm -rf "$TMPDIR"' EXIT
 
 # Archive main repository
 git archive --prefix="RimSort-$VERSION/" HEAD | tar -x -C "$TMPDIR"
 
 # Archive submodules
-git submodule foreach --quiet 'git archive --prefix="RimSort-'"$VERSION"'/$displaypath/" HEAD | tar -x -C "'"$TMPDIR"'"'
+git submodule foreach --quiet "git archive --prefix=\"RimSort-$VERSION/\$displaypath/\" HEAD | tar -x -C \"$TMPDIR\""
 
 # Create the final tarball
 cd "$TMPDIR"
