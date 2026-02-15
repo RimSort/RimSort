fb32be25d40b4eb09697096236eed09eb09bba71
Patryk Malinowski <145374860+Patryk-Malinowski@users.noreply.github.com>
2026-02-15 20:35:21 +0000

    Remove aux db performance mode (#1772)
    I decided there is no need for this as it can only cause issues for
    users and us down the line...
    
    This would only noticeably improve performance for bulk write operations
    which are not that common. If something goes wrong and the aux db gets
    corrupted, a user can lose all their mod coloring and mod notes...

--- app/controllers/settings_controller.py
+++ app/controllers/settings_controller.py
@@ -6,11 +6,9 @@ from pathlib import Path
 from loguru import logger
 from PySide6.QtCore import QObject, Slot
 from PySide6.QtWidgets import QApplication, QLineEdit, QMessageBox
-from sqlalchemy import text
 
 from app.controllers.instance_controller import InstanceController
 from app.controllers.language_controller import LanguageController
-from app.controllers.metadata_db_controller import AuxMetadataController
 from app.controllers.theme_controller import ThemeController
 from app.models.settings import Instance, Settings
 from app.utils.acf_utils import validate_acf_file_exists
@@ -346,13 +344,7 @@ class SettingsController(QObject):
         self.settings_dialog.include_mod_notes_in_mod_name_filter_checkbox.stateChanged.connect(
             self._on_include_mod_notes_in_mod_name_filter_changed
         )
-
-        # Performance tab
-        self._enable_aux_db_performance_mode()
-        self.settings_dialog.aux_db_performance_mode.stateChanged.connect(
-            self._enable_aux_db_performance_mode
-        )
-
+        
         EventBus().settings_have_changed.connect(self._handle_mod_coloring_mode_changed)
 
         # Connect signals from dialogs
@@ -1008,9 +1000,6 @@ class SettingsController(QObject):
         self.settings_dialog.enable_aux_db_behavior_editing.setChecked(
             self.settings.enable_aux_db_behavior_editing
         )
-        self.settings_dialog.aux_db_performance_mode.setChecked(
-            self.settings.enable_aux_db_performance_mode
-        )
         self.settings_dialog.rentry_auth_code.setText(self.settings.rentry_auth_code)
         self.settings_dialog.rentry_auth_code.setCursorPosition(0)
         self.settings_dialog.github_username.setText(self.settings.github_username)
@@ -1352,9 +1341,6 @@ class SettingsController(QObject):
         self.settings.max_backups = self.settings_dialog.max_backups_spinbox.value()
         self.settings.enable_aux_db_behavior_editing = (
             self.settings_dialog.enable_aux_db_behavior_editing.isChecked()
-        )
-        self.settings.enable_aux_db_performance_mode = (
-            self.settings_dialog.aux_db_performance_mode.isChecked()
         )
         self.settings.rentry_auth_code = self.settings_dialog.rentry_auth_code.text()
         self.settings.github_username = self.settings_dialog.github_username.text()
@@ -2470,25 +2456,6 @@ class SettingsController(QObject):
         self.settings.include_mod_notes_in_mod_name_filter = (
             self.settings_dialog.include_mod_notes_in_mod_name_filter_checkbox.isChecked()
         )
-
-    @Slot()
-    def _enable_aux_db_performance_mode(self) -> None:
-        """
-        Enable/disable the auxiliary metadata database performance mode based on the checkbox state.
-        """
-        aux_metadata_controller = AuxMetadataController.get_or_create_cached_instance(
-            self.settings.aux_db_path
-        )
-        with aux_metadata_controller.Session() as session:
-            if self.settings_dialog.aux_db_performance_mode.isChecked():
-                session.execute(text("PRAGMA synchronous = OFF"))
-                session.execute(text("PRAGMA journal_mode = MEMORY"))
-            else:
-                session.execute(text("PRAGMA synchronous = FULL"))
-                session.execute(text("PRAGMA journal_mode = DELETE"))
-
-            session.commit()
-            session.close()
 
     @Slot()
     def _handle_mod_coloring_mode_changed(self) -> None:
