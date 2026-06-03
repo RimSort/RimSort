43209c89dc65c1cf573292b04cbaf66060bb8526
Anten Skrabec <cebarks@gmail.com>
2026-06-03 17:40:26 +0200

    refactor(settings): implement SortingTabController

--- app/controllers/settings_tabs/__init__.py
+++ app/controllers/settings_tabs/__init__.py
@@ -1,3 +1,4 @@
 from app.controllers.settings_tabs.base_tab_controller import BaseTabController
+from app.controllers.settings_tabs.sorting_tab_controller import SortingTabController
 
-__all__ = ["BaseTabController"]
+__all__ = ["BaseTabController", "SortingTabController"]
