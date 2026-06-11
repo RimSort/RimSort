376dad28640605c877b6e5e519659c6d1430b952
Tobias <t-zimmermann@mailbox.org>
2026-06-11 13:20:14 +0200

    Add sorting by when the mods were updated on Steam

--- app/views/mods_panel.py
+++ app/views/mods_panel.py
@@ -999,6 +999,7 @@ class ModListWidget(QListWidget):
         "PackageId": ModsPanelSortKey.PACKAGEID,
         "Color": ModsPanelSortKey.MOD_COLOR,
         "Tags": ModsPanelSortKey.MOD_TAGS,
+        "Mod Updated": ModsPanelSortKey.MOD_UPDATED,
     }
 
     @staticmethod
@@ -3854,6 +3855,7 @@ class ModsPanel(QWidget):
         "PackageId": ModsPanelSortKey.PACKAGEID,
         "Color": ModsPanelSortKey.MOD_COLOR,
         "Tags": ModsPanelSortKey.MOD_TAGS,
+        "Mod Updated": ModsPanelSortKey.MOD_UPDATED,
     }
 
     # Note: combobox items store their corresponding `ModsPanelSortKey` in
@@ -4230,6 +4232,9 @@ class ModsPanel(QWidget):
         )
         self.inactive_mods_sort_combobox.addItem(
             self.tr("Tags"), ModsPanelSortKey.MOD_TAGS
+        )
+        self.inactive_mods_sort_combobox.addItem(
+            self.tr("Mod Updated"), ModsPanelSortKey.MOD_UPDATED
         )
         # Set initial combo box selection based on loaded settings by matching
         # the stored enum name to the item userData. Fall back to
